8c2f1001d353e5ec88ac1b3bdf4458f3f53fffd3
Jori van Ee <jorivanee@hotmail.com>
2023-03-28 12:11:00 +0200

    Update application.py

--- application.py
+++ application.py
@@ -28,8 +28,11 @@ def store_email():
     raw_email = request.form.to_dict()
     headers = {k: v.strip() for k, v in [line.split(
         ":", 1) for line in raw_email['headers'].splitlines() if ":" in line]}
-    entry = {"headers": headers, "text": raw_email['text'].strip(
-    ), "subject": raw_email['subject'], "to": raw_email['to'], "html": raw_email["html"], "sender_ip": raw_email['sender_ip'], "from": raw_email['from'], "envelope": json.loads(raw_email['envelope'])}
+    entry = {"headers": headers, "subject": raw_email['subject'], "to": raw_email['to'],
+             "sender_ip": raw_email['sender_ip'], "from": raw_email['from'], "envelope": json.loads(raw_email['envelope'])}
+    for e in ['text', 'html']:
+        if e in raw_email:
+            entry[e] = raw_email(e)
     app.database.emails.insert_one(entry)
     return jsonify({"success": True}), 202
 
